62444ff686e2c3efaffb7c009fda56b796d7852e
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2024-09-17 17:24:41 +0300

    get to know with send/publsih semantics

--- SimpleConsumerProducer.Producer/Controllers/OrderController.cs
+++ SimpleConsumerProducer.Producer/Controllers/OrderController.cs
@@ -10,16 +10,37 @@ public class OrderController: ControllerBase
 {
     private IPublishEndpoint _endpoint;
     private ILogger<OrderController> _logger;
+    private ISendEndpointProvider _sendEndpointProvider;
 
-    public OrderController(IPublishEndpoint endpoint, ILogger<OrderController> logger)
+    public OrderController(IPublishEndpoint endpoint, ILogger<OrderController> logger, ISendEndpointProvider sendEndpointProvider)
     {
         _endpoint = endpoint;   
         _logger = logger;
+        _sendEndpointProvider = sendEndpointProvider;
     }
 
-    [HttpPost("/order")]
+    [HttpPost("/order-publish")]
     [ProducesResponseType(typeof(OrderCreated),StatusCodes.Status201Created)]
-    public async Task<IActionResult> CreateOrder(CreateOrder order)
+    public async Task<IActionResult> CreateOrderPublish(CreateOrder order)
+    {
+        // validate order somehow, simple one:
+        if (order.Trees <= 0 || order.Trees >= 100)
+        {
+            return BadRequest(new ErrorResponse()
+            {
+                StatusCode = 403,
+                Message = "Введено некорректное число деревьев"
+            });
+        }
+        _logger.LogInformation("Publishing an order creation message for {Trees} trees.", order.Trees);
+        await _endpoint.Publish(order);
+        _logger.LogInformation("Order creation message published.");
+        return Created();
+    }
+    
+    [HttpPost("/order-send")]
+    [ProducesResponseType(typeof(OrderCreated),StatusCodes.Status201Created)]
+    public async Task<IActionResult> CreateOrderSend(CreateOrder order)
     {
         // validate order somehow, simple one:
         if (order.Trees <= 0 || order.Trees >= 100)
@@ -31,7 +52,9 @@ public class OrderController: ControllerBase
             });
         }
         _logger.LogInformation("Sending an order creation message for {Trees} trees.", order.Trees);
-        await _endpoint.Publish(order);
+        // actually the same as publish, but we could send it to the queue directly
+        var endpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri("exchange:Contracts:CreateOrder"));
+        await endpoint.Send(order, context => context.Headers.Set("x-custom-header-test","some-value"));
         _logger.LogInformation("Order creation message sent.");
         return Created();
     }
